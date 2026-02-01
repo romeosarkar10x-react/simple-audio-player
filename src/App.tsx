1d791ff7387307f21b659c700ccca66ce9bb3a4f
Romeo Sarkar <romeosarkar10x@gmail.com>
2026-02-01 18:02:23 +0530

    Added more sounds

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,6 @@ import "./App.css";
 const Sound = forwardRef<HTMLAudioElement, PropsWithoutRef<AudioHTMLAttributes<HTMLAudioElement>> & { src: string }>(
     ({ src, ...props }, ref) => {
         const formatSrc = (src: string) => {
-            if (src.startsWith("/sounds/")) {
-                src = src.slice(8);
-            }
-
             src = src.replaceAll(/([a-z])([A-Z])/g, "$1 $2");
             src = src.toLowerCase();
             src = src[0].toUpperCase() + src.slice(1);
@@ -19,7 +15,7 @@ const Sound = forwardRef<HTMLAudioElement, PropsWithoutRef<AudioHTMLAttributes<H
 
         return (
             <div className="flex items-center my-4 ml-8">
-                <audio ref={ref} {...props} src={`${GLOBALS.BASE_URL}${src}`} controls></audio>
+                <audio ref={ref} {...props} src={`${GLOBALS.BASE_URL}/sounds/${src}.mp3`} controls></audio>
                 <span className="ml-4">{formatSrc(src)}</span>
             </div>
         );
@@ -27,15 +23,25 @@ const Sound = forwardRef<HTMLAudioElement, PropsWithoutRef<AudioHTMLAttributes<H
 );
 
 function App() {
-    const srcs = ["/sounds/clashRoyaleSoundtrack.mp3", "/sounds/goblins.mp3", "/sounds/mortar.mp3"];
+    const srcs = [
+        "arrow",
+        "booHooHooHew",
+        "clashRoyaleSoundtrack",
+        "fireSpirits",
+        "goblins",
+        "kingLaugh",
+        "miniPekka",
+        "mortar",
+        "rocket",
+        "spearGoblins",
+    ];
+
     const audioRefs = useRef<(HTMLAudioElement | null)[]>(Array.from({ length: srcs.length }).map(() => null));
 
     const handlePlayCurry = (index: number) => () => {
-        console.log("handlePlay...");
         audioRefs.current.forEach((audio, i) => {
             if (audio !== null) {
                 if (i !== index) {
-                    console.log(`Pausing ${i}...`);
                     audio.pause();
                 }
             }
