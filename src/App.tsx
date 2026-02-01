93d9bb028608e5ad14bf59ce4ecc18281e324c1a
Romeo Sarkar <romeosarkar10x@gmail.com>
2026-02-01 17:33:59 +0530

    Synchronized audio play

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,56 @@
-import "./App.css";
+import { forwardRef, useRef, type AudioHTMLAttributes, type PropsWithoutRef } from "react";
 import { GLOBALS } from "./globals";
 
-function Sound({ src }: { src: string }) {
-    const formatSrc = (src: string) => {
-        if (src.startsWith("/sounds/")) {
-            src = src.slice(8);
-        }
+import "./App.css";
 
-        src = src.replaceAll(/([a-z])([A-Z])/g, "$1 $2");
-        src = src.toLowerCase();
-        src = src[0].toUpperCase() + src.slice(1);
+const Sound = forwardRef<HTMLAudioElement, PropsWithoutRef<AudioHTMLAttributes<HTMLAudioElement>> & { src: string }>(
+    ({ src, ...props }, ref) => {
+        const formatSrc = (src: string) => {
+            if (src.startsWith("/sounds/")) {
+                src = src.slice(8);
+            }
 
-        return src;
-    };
+            src = src.replaceAll(/([a-z])([A-Z])/g, "$1 $2");
+            src = src.toLowerCase();
+            src = src[0].toUpperCase() + src.slice(1);
 
-    return (
-        <div className="flex items-center my-4 ml-8">
-            <audio src={`${GLOBALS.BASE_URL}${src}`} controls></audio>
-            <span className="ml-4">{formatSrc(src)}</span>
-        </div>
-    );
-}
+            return src;
+        };
+
+        return (
+            <div className="flex items-center my-4 ml-8">
+                <audio ref={ref} {...props} src={`${GLOBALS.BASE_URL}${src}`} controls></audio>
+                <span className="ml-4">{formatSrc(src)}</span>
+            </div>
+        );
+    },
+);
 
 function App() {
     const srcs = ["/sounds/clashRoyaleSoundtrack.mp3", "/sounds/goblins.mp3", "/sounds/mortar.mp3"];
+    const audioRefs = useRef<(HTMLAudioElement | null)[]>(Array.from({ length: srcs.length }).map(() => null));
+
+    const handlePlayCurry = (index: number) => () => {
+        console.log("handlePlay...");
+        audioRefs.current.forEach((audio, i) => {
+            if (audio !== null) {
+                if (i !== index) {
+                    console.log(`Pausing ${i}...`);
+                    audio.pause();
+                }
+            }
+        });
+    };
 
     return (
         <>
-            {srcs.map((src) => (
-                <Sound {...{ src }} />
+            {srcs.map((src, index) => (
+                <Sound
+                    ref={(elem) => void (audioRefs.current[index] = elem)}
+                    onPlay={handlePlayCurry(index)}
+                    key={src}
+                    {...{ src }}
+                />
             ))}
         </>
     );
